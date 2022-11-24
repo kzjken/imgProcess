a801d8aa3b6e242f02d23a65b4112febf221ea44
ZKang <kzjken@gmail.com>
2022-11-24 23:00:28 +0100

    review, clean up and  optimize

--- imgProcessGUI.py
+++ imgProcessGUI.py
@@ -7,6 +7,8 @@ import os
 import glob
 import imgProcess
 import sys
+from datetime import datetime
+from datetime import date
 
 #################################################################################################################################
 #------------------------------------------------------------- Global ----------------------------------------------------------#
@@ -34,6 +36,9 @@ class PrintLogger():
 #################################################################################################################################
 root = Tk()
 root.title("Image Converter [Z.Kang]")
+root.geometry("860x540")
+root.minsize(860, 540)
+root.maxsize(860, 540)
 
 mainframe = ttk.Frame(root, padding = "20 3 12 12")
 mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
@@ -41,7 +46,7 @@ root.columnconfigure(0, weight = 1)
 root.rowconfigure(0, weight = 1)
 
 #################################################################################################################################
-# row 0 path
+# row 0 source path
 #################################################################################################################################
 ttk.Label(mainframe, text = "Source path:").grid(row = 0, column = 0, sticky = E, padx = 5, pady = 5)
 
@@ -52,13 +57,13 @@ srcPath_entry.grid(row = 0, column = 1, sticky = (W, E), padx = 5, pady = 5)
 def selectSrcDir():
     filepath = os.path.normpath(filedialog.askdirectory())
     srcPath.set(filepath)
-    #destPath.set(filepath + "/" + os.path.basename(filepath) + "_Output")
+    # set destination path = source path + _OUT
     if(os.path.exists(filepath)):
         destPath.set(filepath + "_OUT")
 ttk.Button(mainframe, text = "Browse", command = selectSrcDir).grid(row = 0, column = 2, sticky = W, padx = 5, pady = 5)
 
 #################################################################################################################################
-# row 1 path
+# row 1 destination path
 #################################################################################################################################
 ttk.Label(mainframe, text = "Destination path:").grid(row = 1, column = 0, sticky = E, padx = 5, pady = 5)
 
@@ -72,17 +77,18 @@ def modDestDir():
 ttk.Button(mainframe, text = "Change", command = modDestDir).grid(row = 1, column = 2, sticky = W, padx = 5, pady = 5)
 
 #################################################################################################################################
-# row 2 - 4
+# row 2 - 3: Functions
 #################################################################################################################################
 ttk.Label(mainframe, text = "Functions:").grid(row = 2, column = 0, sticky = E, padx = 5, pady = 5)
 
+""" rename """
 renameFlag = StringVar()
 renameFlag.set(1)
 rename_checkbutton = ttk.Checkbutton(mainframe, text = "Rename", variable = renameFlag)
 rename_checkbutton.grid(row = 2, column = 2, sticky = (W, E), padx = 5)
-
 ttk.Label(mainframe, text = "1. Rename photos to Shotdata_Shotdevice").grid(row = 2, column = 1, sticky = W, padx = 5, pady = 5)
 
+""" compress """
 compressFlag = StringVar()
 compressFlag.set(1)
 compress_checkbutton = ttk.Checkbutton(mainframe, text = "Compress", variable = compressFlag)
@@ -109,86 +115,104 @@ sys.stdout = pl
 #################################################################################################################################
 # row 5 execute
 #################################################################################################################################
-def checkPath(srcFolder, destfolder):
+""" check if src and dest paths are valid """
+def checkPath(srcFolder, destFolder):
+    print(date.today().strftime("%Y.%m.%d") + ' ' + datetime.now().strftime("%H:%M:%S"))
+    print("---------------------------------------------------------------------------------------")
     print("check path:")
+
+    # check if source path is empty, exists or valid...
     if srcFolder == "":
-        #messagebox.showerror(title = "错误", message = "指定源文件路径无效，请重新选择！")
+        print("  Error: Source path is empty, please select...")
+        return False
+    if not os.path.exists(srcFolder):
+        print("  Error: Source path doesn't exist, please reselect...")
+        return False
+    if not os.path.isdir(srcFolder):
         print("  Error: Source path invalid, please reselect...")
         return False
 
     # check if source folder is empty
     if not os.listdir(srcFolder):
-        #messagebox.showerror(title = "错误", message = "目标文件夹\n" + srcFolder + "\内没有文件！")
         print("  Error: No images in source folder, please reselect...")
         return False
     else:
-        if destfolder == "":
-            #messagebox.showerror(title = "错误", message = "指定目标文件路径无效，请重新选择！")
+        if destFolder == "" or not os.path.isdir(destFolder):
             print("  Error: Destination path invalid, please reselect...")
             return False
         else:
             return True
-            # if (os.path.exists(destfolder) == False):
-            #     if destfolder == srcFolder + "_OUTPUT":
-            #         os.makedirs(destfolder)
-            #         print("  目标文件夹" + destfolder + "已创建。")
-            #         return True
-            #     else:
-            #         msgBoxReturn = messagebox.askquestion(title = "提示", message = "目标文件夹\n" + destfolder + "\n不存在, 是否创建？")
-            #         if msgBoxReturn == "yes":
-            #             os.makedirs(destfolder)
-            #             return True
-            #         else:
-            #             return False
-            # else:
-            #     return True
 
+###########################################################
+
+""" check if functions are selected """
 def checkCheckButton():
+    invalidCounter = 0
     print("Check selected options:")
-    #print(renameFlag.get() + compressFlag.get() + "   " + type(renameFlag.get()))
+    # print(renameFlag.get() + compressFlag.get())
 
-    if renameFlag.get() == '0' and compressFlag.get() == '0':
-        print("  Error: Please select at least one function...")
+    if renameFlag.get() == '1':
+        print("  Rename checked.")
+    else:
+        print("  Rename unchecked.")
+        invalidCounter += 1# invalidCounter + 1
+
+    if compressFlag.get() == '1':
+        print("  Compress checked.")
+    else:
+        print("  Compress unchecked.")
+        invalidCounter += 1
+
+    # if renameFlag.get() == '0' and compressFlag.get() == '0':
+    if invalidCounter > 1:
+        print("Error: Please select at least one function...")
         return False
     else:
         return True
 
-def preview(srcFolder, destfolder, extName):
+###########################################################
+
+""" preview changes """
+def preview(srcFolder, destFolder, extName):
     srcPathIncExtName = srcFolder + "\\*." + extName
-    filecounter = 0
+    fileCounter = 0
     srcList = []
     destList = []
+
+    # glob extName e.g. jpg files in srcFolder
     for srcName in glob.glob(srcPathIncExtName):
+        # rename preview
         if renameFlag.get() == '1':
             exifList = imgProcess.getExif(srcName)
-            destName = destfolder + "\\" + imgProcess.renameAccExif(srcName, exifList)
+            destName = destFolder + "\\" + imgProcess.renameAccExif(srcName, exifList)
         else:
-            destName = destfolder + "\\" + os.path.basename(srcName)
-
+            destName = destFolder + "\\" + os.path.basename(srcName)
         srcList.append(srcName)
         destList.append(destName)
-        if renameFlag.get() == '1':
-            print("  " + str(filecounter + 1) + ': ' + os.path.basename(srcName) + " ==> " + os.path.basename(destName))
-        else:
-            print("  " + str(filecounter + 1) + ': ' + os.path.basename(srcName))
-        filecounter += 1
 
-    if filecounter > 0:
-        print("\n" + str(filecounter) + " " + extName + "files found.")
+        # print rename behavior
+        if renameFlag.get() == '1':
+            print("  " + str(fileCounter + 1) + ': ' + os.path.basename(srcName) + " ==> " + os.path.basename(destName))
+        else:
+            print("  " + str(fileCounter + 1) + ': ' + os.path.basename(srcName))
+        fileCounter += 1
+
+    if fileCounter > 0:
+        print("\n" + str(fileCounter) + " " + extName + " files found.")
         print("---------------------------------------------------------------------------------------")
 
+        """ name conflict """
         if renameFlag.get() == '1':
             dupItemIndex = []
             dupItem = []
             for index, element in enumerate(destList):
                 if destList.count(element) > 1:
                     if element not in dupItem:
-                        print("Conflicts:" + str(destList.count(element)) + " " + os.path.basename(element) + ": suffix _X will be added as follow:")
+                        print("Conflicts:" + str(destList.count(element)) + " " + os.path.basename(element) + ": suffix _X will be added.")
                         dupItem.append(element)
                     dupItemIndex.append(index)
 
             if len(dupItem) > 0:
-                # print("结果如下：")
                 for item in dupItem:
                     suffix = 0
                     for index in dupItemIndex:
@@ -207,55 +231,26 @@ def preview(srcFolder, destfolder, extName):
         print("---------------------------------------------------------------------------------------")
         print("Please click execute to continue processing.")
 
-    return filecounter, srcList, destList
+    return fileCounter, srcList, destList
 
-def cpRenImage(srcFolder, destfolder, extName):
-    srcPathIncExtName = srcFolder + "\\*." + extName
-    filecounter = 0
-    for srcName in glob.glob(srcPathIncExtName):
-        if renameFlag.get() == '1':
-            destName = destfolder + "\\" + imgProcess.renameAccExif(srcName)
-        else:
-            destName = destfolder + "\\" + os.path.basename(srcName)
-
-        if os.path.exists(destName):
-            msgBoxReturn = messagebox.askquestion(title = "!!!", message = os.path.basename(destName) + " exits, overwrite？")
-            if msgBoxReturn == "yes":
-                if compressFlag.get() == '1':
-                    imgProcess.renAndcompImg(srcName, destName)
-                    print(str(filecounter + 1) + ': ' + os.path.basename(srcName) + " => " + os.path.basename(destName) + " : renamed and compressed")
-                else:
-                    os.system("copy  " + srcName + " " + destName)
-                    print(str(filecounter + 1) + ': ' + os.path.basename(srcName) + " => " + os.path.basename(destName) + " : renamed")
-                filecounter += 1
-            else:
-                print(str(filecounter + 1) + ': ' + os.path.basename(srcName) + " => " + os.path.basename(destName) + " : canceled")
-        else:
-            if compressFlag.get() == '1':
-                imgProcess.renAndcompImg(srcName, destName)
-                print(str(filecounter + 1) + ': ' + os.path.basename(srcName) + " => " + os.path.basename(destName) + " : renamed and compressed")
-            else:
-                os.system("copy  " + srcName + " " + destName)
-                print(str(filecounter + 1) + ': ' + os.path.basename(srcName) + " => " + os.path.basename(destName) + " : renamed")
-            filecounter += 1
-    return filecounter
+###########################################################
 
 srcListJPG = []
 destListJPG = []
-srcListJPEG = []
-destListJPEG = []
-srcListPNG = []
-destListPNG = []
-def preperation():
+# srcListJPEG = []
+# destListJPEG = []
+# srcListPNG = []
+# destListPNG = []
+
+###########################################################
+
+def previewBtn():
     log_text.configure(state = "normal")
     log_text.delete('1.0', END)
 
-    ### path
+    # path
     srcPath = srcPath_entry.get()
     destPath = destPath_entry.get()
-    # print("源文件夹 = " + srcPath)
-    # print("目标文件夹 = " + destPath)
-    # print("=======================================================================")
 
     if checkPath(srcPath, destPath):
         print("OK")
@@ -265,29 +260,32 @@ def preperation():
             print("function checked: ok.")
             print("=======================================================================================")
             print("Preview:")
-            ############### mainfunction ################
+
             sumJPG, srcList, destList = preview(srcPath, destPath, "jpg")
+            """ for other extension, e.g. jpeg """
+            #sumJPEG, srcListJPEG, destListJPEG = preview(srcPath, destPath, "jpeg")
 
             srcListJPG.clear()
             destListJPG.clear()
+
             for item in srcList:
                 srcListJPG.append(item)
             for item in destList:
                 destListJPG.append(item)
 
-            #sumJPEG, srcListJPEG, destListJPEG = preview(srcPath, destPath, "jpeg")
-            #process_Button.configure(text = "执行操作", command = process)
         else:
             print("=======================================================================================")
-            print("Abort 1!")
+            print("Abort, function check failed!")
             pass
     else:
         print("=======================================================================================")
-        print("Abort 2!")
+        print("Abort, path check failed!")
         pass
 
     log_text.see(END)
     log_text.configure(state = "disable")
+
+###########################################################
 
 def process():
     log_text.configure(state = "normal")
@@ -302,19 +300,14 @@ def process():
         if destPath == srcPath + "_OUT":
             os.makedirs(destPath)
         else:
-            msgBoxReturn = messagebox.askquestion(title = "Message", message = "Destination \n" + destPath + "\ndoesn't exist, create？")
+            msgBoxReturn = messagebox.askquestion(title = "Message", message = "Destination \n" + destPath + "\ndoesn't exist, create?")
             if msgBoxReturn == "yes":
                 os.makedirs(destPath)
                 print("  Create destination folder: " + destPath)
-    print("---------------------------------------------------------------------------------------")
 
     process_Button.configure(state = "disable")
     #compress_checkbutton.configure(state = "normal")
     #rename_checkbutton.configure(state = "normal")
-
-    # print(compressFlag.get())
-    # print(srcListJPG)
-    # print(destListJPG)
 
     if compressFlag.get() == '1':
         for index, imageJPG in enumerate(srcListJPG):
@@ -332,34 +325,15 @@ def process():
     log_text.see(END)
     log_text.configure(state = "disable")
 
-preperation_Button = ttk.Button(mainframe, text = "Preview", command = preperation)
-preperation_Button.grid(row = 4, column = 2, sticky = (N, S), padx = 5, pady = 10)
+###########################################################
+###########################################################
+
+preview_Button = ttk.Button(mainframe, text = "Preview", command = previewBtn)
+preview_Button.grid(row = 4, column = 2, sticky = (N, S), padx = 5, pady = 10)
 
 process_Button = ttk.Button(mainframe, text = "Execute", command = process, state = "disable")
 process_Button.grid(row = 5, column = 2, sticky = (N, S), padx = 5, pady = 5, rowspan = 3)
 
-#################################################################################################################################
-# row 7 -
-#################################################################################################################################
-# def breakProcess():
-#     sys.exit("Stop")
-
-# ttk.Button(mainframe, text = "中止操作", command = breakProcess).grid(row = 8, column = 2, sticky = N, padx = 5, pady = 0)
-# ttk.Button(mainframe, text = "是").grid(row = 9, column = 2, sticky = N, padx = 5, pady = 0)
-# ttk.Button(mainframe, text = "否").grid(row = 10, column = 2, sticky = N, padx = 5, pady = 0)
-#ttk.Button(mainframe, text = "yes").grid(row = 9, column = 2, sticky = N, padx = 5, pady = 0)
-
-
-
-#ttk.Label(mainframe, text = "Z.Kang").grid(row = 5, column = 2, sticky = (S, E), padx = 5, pady = 5)
-
-# # for child in mainframe.winfo_children():
-# #     child.grid_configure(padx = 5, pady = 5)
-
-# feet_entry.focus()
-# root.bind("<Return>", calculate)
-
 root.mainloop()
-
 
 #pyinstaller --noconsole imgProcessGUI.py
